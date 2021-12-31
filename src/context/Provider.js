5b298e24fcf9dc28b2e0772c3cb8f8b433d2f264
LuisCarlosCruz <luizcruz0809@gmail.com>
2021-12-30 21:53:21 -0300

    config inicial prop-types

--- src/context/Provider.js
+++ src/context/Provider.js
@@ -1,5 +1,6 @@
 import Context from './Context';
 import React, { useState } from 'react';
+import PropTypes from 'prop-types';
 
 const Provider = ({ children }) => {
   const [emailIsValid, setEmailIsValid] = useState(false);
@@ -13,3 +14,7 @@ const Provider = ({ children }) => {
 };
 
 export default Provider;
+
+Provider.propTypes = {
+  children: PropTypes.element,
+};
